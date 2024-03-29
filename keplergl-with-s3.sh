1638728ba8569a0b2d5e988df49b791c5ed82e9f
jvstinian <jvstinian@gmail.com>
2024-03-29 08:45:53 -0400

    docker-compose and supporting scripts (#4)
    * Adding docker-compose files and scripts.
    * Improving script for packaging base images.

---
+++ keplergl-with-s3.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+display_help() {
+  cat << EOF
+Name:
+  keplergl-with-s3 - CLI tool for this project
+
+Commands:
+  initialize               - installs yarn packages for web servers and builds base images
+EOF
+}
+
+if [ "$#" -eq 0 ]; then
+  display_help
+  exit 1
+fi
+
+COMMAND="$1"
+case "$COMMAND" in
+  initialize)
+    "$DIR/scripts/setup-local-node-servers.sh"
+    source "$DIR/scripts/package-base-images.sh" && build_all_base_images
+  ;;
+  *)
+    display_help
+    echo -e "Unknown Command: $COMMAND\\n"
+    exit 1
+esac
